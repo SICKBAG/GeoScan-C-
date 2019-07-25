06aca657b4f17fa2bf85821144ded88de3efcd10
Artorias <danil-5585@mail.ru>
2019-07-25 13:17:04 +0500

    Add files via upload

---
+++ GeoScan/GeoScan/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace GeoScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(466, 304);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(458, 278);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Analisys";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(458, 278);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Scan";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 74);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(445, 169);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 249);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(445, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Unload";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(254, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Get File";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(445, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 328);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GeoScan/GeoScan/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using FastSearchLibrary;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace GeoScan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static object locker = new object(); 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> fileExts = new List<string>() {"?.SHP", "?.shx", "?.DBF", "?.PRJ", "?.CBN", "?.XML", "?.MIF", "?.Mid", "?.Tab", "?.KML", "?KMZ", "?.GPS", "*.tiff", "?.jpg", "?.jpeg", "?.geotiff"};
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            List<FileInfo> files = new List<FileInfo>(); // список результатов
+            foreach (string fileExt in fileExts)
+            {
+                FileSearcher searcher = new FileSearcher(@"C:\", fileExt, tokenSource);
+                searcher.FilesFound += (sndr, arg) =>
+                     {
+                         lock (locker)
+                         {
+                             Invoke((Action)(() =>
+                             {
+                                 arg.Files.ForEach((f) =>
+                                 {
+                                     textBox2.Text += "Расположение: " + f.FullName + Environment.NewLine + "Дата создания: " + f.CreationTime + Environment.NewLine + "Дата последнего изменения: " +
+                                         f.LastWriteTime + Environment.NewLine;
+                                     files.Add(f);
+                                 });
+                             }));
+
+                         }
+                     };
+                searcher.SearchCompleted += (sndr, arg) =>
+                {
+                    if (arg.IsCanceled)
+                        textBox2.Text += "Поиск остановлен" + Environment.NewLine;
+                    else
+                        Invoke((Action)(() =>
+                            {
+                                textBox2.Text += "Поиск завершён" + Environment.NewLine;
+                            }));
+                    ;
+                    Invoke((Action)(() =>
+                        {
+                            int fCount = files.Count;
+                            textBox2.Text += "Найдено файлов: " + fCount + Environment.NewLine;
+                            fCount = 0;
+                        }));
+                };
+                var task = searcher.StartSearchAsync();
+                DriveInfo[] driveNames = DriveInfo.GetDrives();
+                char[] deaf = { ':', '\\' };
+                string result = null;
+                string compName = Environment.UserName;
+                foreach (DriveInfo driveName in driveNames)
+                {
+                    result = Convert.ToString(driveName);
+                    result = result.Replace(":", "");
+                    result = result.Replace("\\", "");
+                    driveName.ToString();
+                    StreamWriter file = new StreamWriter("C:\\Users\\-\\Desktop\\" + Environment.UserName + "_" + result + ".csv");
+                    file.WriteLine(textBox2.Text);
+                    file.Close();
+                }
+            }
+           /* string compName = Environment.UserName;
+            textBox2.Text = compName + Environment.NewLine;
+            SearchFiles();
+            //foreach (string file in Directory.GetFiles("C:\\", string.Format("*.{0}", fileType), SearchOption.AllDirectories))
+            string dirName = "C:\\";
+            string[] files = Directory.GetFiles(dirName);
+            if (files.Length > 0)
+            {
+                textBox2.Text += "Files" + Environment.NewLine;
+                foreach (string file in files)
+                {
+                    FileInfo fileInf = new FileInfo(file);
+                    textBox2.Text += file;
+                    textBox2.Text += Environment.NewLine + "Creation time" + Environment.NewLine + fileInf.CreationTime + Environment.NewLine + "Length" + Environment.NewLine + fileInf.Length +
+                    Environment.NewLine + "Last write time" + Environment.NewLine + fileInf.LastWriteTime + Environment.NewLine;
+                }
+            }
+            else textBox2.Text += "Files not found" + Environment.NewLine;
+            string[] dirs = Directory.GetDirectories(dirName);
+            if (dirs.Length > 0)
+            {
+                foreach (string dir in dirs)
+                {
+                    textBox2.Text += dir + Environment.NewLine;
+                }
+            }
+            DirectoryInfo dirInfo = new DirectoryInfo();
+            List<FileInfo> files = new List<FileInfo>();
+            string[] fileTypes = new string[] { "shp", "shx", "dbf", "prj", "cbn", "xml", "MIF", "MID", "tab", "kml", "kmz", "gps", "map", "tif", "tiff", "jpg", "jpeg", "geotiff" };
+            foreach (DriveInfo drive in drives)
+            {
+                foreach (string dir in Directory.EnumerateFiles(drive,"?.{0}", fileTypes, SearchOption.AllDirectories))
+             * {
+             *      DirectoryInfo dirInfo = new DirectoryInfo(dir);
+             *      if ((dirInfo.Attributes & FileAttributes.Hidden)==0)
+             *      {
+             *          
+             *      }
+             * }
+            }
+            foreach (string subDir in Directory.GetDirectories("C:\\"))
+            {
+                try
+                {
+                    GFiles(subDir);
+                }
+                catch
+                {
+                }
+            }
+             if (File.Exists(dir))
+             * {     
+             * }
+             * if (Directory.Exists(dir))
+             * {
+             * }
+             */
+
+        }
+        /*private void SearchFiles()
+        {
+            List<string> dirPath = new List<string>();
+            List<string> dirNames = new List<string>();
+            List<string> subDirNames = new List<string>();
+            string dirName = "C:\\";
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+            string[] files = Directory.GetFiles(dirName);
+            if (files.Length > 0)
+            {
+                textBox2.Text += "Files" + Environment.NewLine;
+                foreach (string file in files)
+                {
+                    FileInfo fileInf = new FileInfo(file);
+                    textBox2.Text += file;
+                    textBox2.Text += Environment.NewLine + "Creation time" + Environment.NewLine + fileInf.CreationTime + Environment.NewLine + "Length" + Environment.NewLine + fileInf.Length +
+                    Environment.NewLine + "Last write time" + Environment.NewLine + fileInf.LastWriteTime + Environment.NewLine;
+                }
+            }
+            else
+            {
+                textBox2.Text += "Files not found" + Environment.NewLine;
+            }
+            List<string> dirs = new List<string>();
+            string[] dirsC = Directory.GetDirectories(dirName);
+            dirs.Add(dirsC.ToString());
+            if (dirs.Count > 0)
+            {
+                foreach (string dir in dirs)
+                {
+                    if ((dirInfo.Attributes & FileAttributes.Hidden) == 0)
+                    {
+                        dirPath.Add(dirInfo.FullName);
+                        dirNames.Add(dir);
+                        subDirNames.Clear();
+                        dirs.Clear();
+                        
+                    }
+                }
+            }
+            if (dirNames.Count > 0)
+            {
+                foreach (string dir in dirNames)
+                {
+                    if ((dirInfo.Attributes & FileAttributes.Hidden) == 0)
+                    {
+                        dirName =  dir;
+                        dirPath.Add(dirInfo.FullName);
+                        subDirNames.Add(dir);
+                        dirNames.Clear();
+                    }
+                }
+            }
+            if (subDirNames.Count > 0)
+            {
+                foreach (string dir in subDirNames)
+                {
+                    if ((dirInfo.Attributes & FileAttributes.Hidden) == 0)
+                    {
+                        dirName = dir;
+                        dirPath.Add(dirInfo.FullName);
+                        dirNames.Add(dir);
+                    }
+                }
+            }
+            SearchFiles();
+        }*/
+    }
+}
